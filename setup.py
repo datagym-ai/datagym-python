343165c39ef440ecba11711928d2208c08e3095e
johannes <johannes.pflugmacher@eforce21.com>
2020-03-20 11:53:30 +0100

    LAB-380: Bumped Version to 0.1.2a for Test PyPI

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ test_requirements = ['pytest>=3', ]
 
 setuptools.setup(
    name='datagym',
-   version='0.1.2',
+   version='0.1.2a',
    description='Datagym Python API Wrapper',
    author='Alexej Penner, Johannes Pflugmacher',
    author_email='support@datagym.ai',
