643bb3a4329b1b54c6b1d09c3bc075ae4a615762
Alexej <alexej.penner@eforce21.com>
2020-04-27 15:25:48 +0200

    Update to version 0.3.0, added coco importer, updated dataset to not need owner

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ test_requirements = ['pytest>=3', ]
 
 setuptools.setup(
    name='datagym',
-   version='0.2.0',
+   version='0.3.0',
    description='Datagym Python API Wrapper',
    author='Johannes Pflugmacher',
    author_email='support@datagym.ai',
